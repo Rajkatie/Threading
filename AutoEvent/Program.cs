7fa5070fad4de6fc56c0803ec33748ad952a8346
Rajesh <rajkatie@gmail.com>
2017-03-24 20:43:59 +0530

    Add project files.

---
+++ AutoEvent/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading;
+
+namespace AutoEvent
+{
+    public class Program
+    {
+        static AutoResetEvent autoEvent = new AutoResetEvent(false);
+
+        static void Main(string[] args)
+        {
+            new Thread(SayHello).Start();
+
+            Console.ReadLine();
+
+            autoEvent.Set();
+
+            Console.ReadKey();
+
+        }
+
+        private static void SayHello()
+        {
+            Console.WriteLine("starting: " + Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine("Awaiting for signaling");
+            autoEvent.WaitOne();
+            Console.WriteLine("finshing");
+        }
+    }
+}
---
+++ Threading/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+
+namespace Threading
+{
+   public class Program
+    {
+       static EventWaitHandle handle = new EventWaitHandle(false, EventResetMode.ManualReset, "localhost");
+        static void Main(string[] args)
+        {
+            Console.WriteLine("main thread Id: " + Thread.CurrentThread.ManagedThreadId);
+
+            Thread t1 = new Thread(SayHello);
+            t1.Start("hello");
+           
+            new Thread(SayHello).Start("rajesh");
+            new Thread(SayHello).Start("patel");
+
+            Thread.Sleep(3000);
+            handle.Set();
+
+            //Thread.Sleep(5000);
+            //handle.Set();
+
+            Console.ReadKey();
+        }
+
+        private static void SayHello(object printMessage)
+        {
+            Console.WriteLine("inside method: " + printMessage + " : "+ Thread.CurrentThread.ManagedThreadId);
+
+            handle.WaitOne();
+
+            Console.WriteLine(printMessage);
+        }
+    }
+}
